66879c0fd527ac484b17f4a0f49eb00651d8cbc2
Andrey Pleshkov <and-pleshkov-1996@yandex.ru>
2014-11-13 21:19:53 +0300

    Update OP-1.py

--- OP-1.py
+++ OP-1.py
@@ -1,54 +1,52 @@
-#! Все для Линуксоидов)))
+info_help = '''
+здесь будут инструкции для пользователя
+'''     # Это будет храниться в файле!
+        # P.C. ДОБАВИТЬ СЛОВ!!!
+        # Это не работает, это ПыЖиК.
+        # Это хелпер проги... его еще пилить и пилить
+        #! ДОПИСАТЬ. И разобраться, как там изменять шрифты и все такое прочее.
 
-shelp='''
-HELP ME!!!
-
+info_about = '''
 В создании этой програмы принимали участие:
 Плешков Андрей
 Жукова Оксана
 Кравчук Павел
+'''      # Это будет храниться в файле!
 
-Это не работает, это ПыЖиК.
+database = [
+    ['Борботько','Санкт-Петербург','89067778734','12.04.1996'],
+    ['Воробьева','Санкт-Петербург','84035793405','28.02.1996'],
+    ['Жукова','Таганрог','89819532406','25.11.1995'],
+    ['Зименко','Санкт-Петербург','89476389479','18.12.1995'],
+    ['Кравчук','Мурманск','89086068965','12.12.1995'],
+    ['Плешков','Пушкин','89078970896','09.05.1996'],
+    ['Бурков','Чебоксары','84123454051','01.02.1996'],
+    ['Гончар','Москва','89834565544','15.11.1995'],
+    ['Гашков','Пермь','89061111911','30.10.1996'],
+    ['Неменчинский','Красноярск','89086068965','12.12.1995'],
+    ['Фурсов','Санкт-Петербург','89052220917','12.03.1997']
+]           #! Это должно храниться в файле!
 
-P.C. ДОБАВИТЬ СЛОВ!!!
-      ''' # Это хелпер проги... его еще пилить и пилить
-          #! ДОПИСАТЬ. И разобраться, как там изменять шрифты и все такое прочее.
+lowercases = '''йцукенгшщзхъфывапролджэячсмитьбю'''
+uppercases = '''ЙЦУКЕНГШЩЗХФЫВАПРОЛДЖЭЯЧСМИТБЮ'''
+numerals = '''0123456789'''
+sign = '''- '''
 
-s = [['Борботько','Санкт-Петербург','89067778734','12.04.1996'],
-     ['Воробьева','Санкт-Петербург','84035793405','28.02.1996'],
-     ['Жукова','Таганрог','89819532406','25.11.1995'],
-     ['Зименко','Санкт-Петербург','89476389479','18.12.1995'],
-     ['Кравчук','Мурманск','89086068965','12.12.1995'],
-     ['Плешков','Пушкин','89078970896','09.05.1996'],
-     ['Бурков','Чебоксары','84123454051','01.02.1996'],
-     ['Гончар','Москва','89834565544','15.11.1995'],
-     ['Гашков','Пермь','89061111911','30.10.1996'],
-     ['Неменчинский','Красноярск','89086068965','12.12.1995'],
-     ['Фурсов','Санкт-Петербург','89052220917','12.03.1997']]
 
-# Список...
-#! Это должно храниться в файле!
-
-alfm = '''йцукенгшщзхъфывапролджэячсмитьбю'''   # Алфавит строчных символов
-alfb = '''ЙЦУКЕНГШЩЗХФЫВАПРОЛДЖЭЯЧСМИТБЮ'''     # Алфавит заглавных символов
-alfz = '''- '''                                 # Алфавит специальных знаков
-
-alf0 = '''0123456789''' # Алфавит цифр
-
-def prfg(line):     # Функция проверки фамилии и города
+def check_name_citi(line):
     log = False
     if len(line)>0:
-        if line[0] in alfb:
+        if line[0] in uppercases:
             log = True
             z = 0
             i = 1
             while i<(len(line)-1):
-                if ((line[i] in alfm) or (line[i] == "'")) and (z == 0):
+                if ((line[i] in lowercases) or (line[i] == "'")) and (z == 0):
                     i += +1
-                elif (line[i] in alfz) and (z == 0):
+                elif (line[i] in sign) and (z == 0):
                     i += +1
                     z = 1
-                elif (line[i] in alfb) and (z == 1):
+                elif (line[i] in uppercases) and (z == 1):
                     i += +1
                     z = 0
                 else:
@@ -57,27 +55,27 @@ def prfg(line):     # Функция проверки фамилии и горо
     
     return(log)
 
-def prte(line):     # Функция проверки номера телефона !ПЕРЕДЕЛАТЬ!
+def check_phone(line):     # !ПЕРЕДЕЛАТЬ!
     log = False
     if len(line) == 11:
         i = 0
-        while (i<len(line)) and (line[i] in alf0):
+        while (i<len(line)) and (line[i] in numerals):
             i += 1
         if i == len(line):
             log = True
             
     return(log)
 
-def prda(line):     # Функция проверки даты рождения
+def check_date(line):
     q1 = [1,3,5,7,8,10,12]
     q2 = [4,6,9,11]
     log = False
     if len(line) == 10:
         if (line[2] == '.') and (line[5] == '.'):
-            if (line[0] in alf0) and (line[1] in alf0):
-                if (line[3] in alf0) and (line[4] in alf0):
-                    if (line[6] in alf0) and (line[7] in alf0) and (line[8] in alf0) and (line[9] in alf0):
-                        q = int(line[9])+10*(int(line[8])+10*(int(line[7])+10*(int(line[6]))))
+            if (line[0] in numerals) and (line[1] in numerals):
+                if (line[3] in numerals) and (line[4] in numerals):
+                    if (line[6] in numerals) and (line[7] in numerals) and (line[8] in numerals) and (line[9] in numerals):
+                        q = int(line[9]) + 10*(int(line[8]) + 10*(int(line[7]) + 10*(int(line[6]))))
                         if 1900<q<2014:
                             if (int(line[4])+10*(int(line[3]))) in q1:
                                 if (int(line[1])+10*(int(line[0])))<32:
@@ -99,74 +97,76 @@ def prda(line):     # Функция проверки даты рождения
 
     return(log)
 
-def Exit():      # А эта замячательная вещь закрывает прогу.
-    ok.destroy() # Разрушаем главное окно, после него прога сама закроется
+def exit():
+    main_window.destroy()
 
 # Переделать функцию выхода: типа, а не хотите ли вы это сохранить?
 
-def Help():                                              # Это та функция, что вызывает хелер
-    vsok = Toplevel(ok)                                  # Создается зависимое окно
-    vsok.title("Help")                                   # Называю его почеловечески...
-    lab = Label(vsok, text = shelp, font = "Courier 14") # Куда, что, каким шрифтом. Именно так.
-    lab.pack()                                           # Пакуем, а то не появится.
+def help():
+    window = Toplevel(main_window)
+    window.title("Справка")
+    lab = Label(window, text = info_help, font = "Courier 14")
+    lab.pack()
 
-def chet():
-    li=123
+def about():
+    window = Toplevel(main_window)
+    window.title("О программе")
+    lab2 = Label(window, text = info_about, font = "Courier 14") #переименовать lab2
+    lab2.pack()    
 
-def poisk(event):
-    p = ent.get()
-    ent.delete(0,END)
+def poisk(event): #переименовать
+    p = line_found.get()
+    line_found.delete(0,END)
     v = []
     i = 0
-    for i in range(0,len(s)):
+    for i in range(0,len(database)):
         k = 0
         while k<4:
-            if p in s[i][k]:
+            if p in database[i][k]:
                 v += [i]
                 k = 4
             k += 1
 
-    vu(s,v)
-
-
-def dob():
-
-    def out(event):
-        vsok.destroy()
+    vu(database,v)
     
-    def proverka(event):
+def DOB():
+
+    def out(event): #переименовать
+        window.destroy()
+    
+    def proverka(event): #переименовать
 
         pr1 = envs1.get()
         pr2 = envs2.get()
         pr3 = envs3.get()
         pr4 = envs4.get()
         d = []
-        global s
+        global database
 
         z = True
         
-        if prfg(pr1) == True:
+        if check_name_citi(pr1) == True:
             d += [pr1]
         else:
             envs1.delete(0,END)
             envs1.insert(END,"Не верно")
             z = False
 
-        if (prfg(pr2) == True) and z:
+        if (check_name_citi(pr2) == True) and z:
             d += [pr2]
         else:
             envs2.delete(0,END)
             envs2.insert(END,"Не верно")
             z = False
             
-        if (prte(pr3) == True) and z:
+        if (check_phone(pr3) == True) and z:
             d += [pr3]
         else:
             envs3.delete(0,END)
             envs3.insert(END,"Не верно")
             z = False
                     
-        if (prda(pr4) == True) and z:
+        if (check_date(pr4) == True) and z:
             d += [pr4]
         else:
             envs4.delete(0,END)
@@ -174,26 +174,26 @@ def dob():
             z = False
 
         if z:
-            s += [d]
-            vsok.destroy()
+            database += [d]
+            window.destroy()
                         
             p = []
-            for i in range(0,len(s)):
+            for i in range(0,len(database)):
                 p += [i]
 
-            vu(s,p)
+            vu(database,p)
 
    
-    vsok = Toplevel(ok)
-    vsok.title("Добавление записи")
-    vsok.minsize(width = 365,height = 150)
-    vsok.maxsize(width = 365,height = 150)
+    window = Toplevel(main_window)
+    window.title("Добавление записи")
+    window.minsize(width = 365,height = 150)
+    window.maxsize(width = 365,height = 150)
 
-    fra1 = Frame(vsok, bg="lightblue")
-    fra2 = Frame(vsok, bg="lightblue")
-    fra3 = Frame(vsok, bg="lightblue")
-    fra4 = Frame(vsok, bg="lightblue")
-    fra5 = Frame(vsok, bg="lightblue")
+    fra1 = Frame(window, bg="lightblue")
+    fra2 = Frame(window, bg="lightblue")
+    fra3 = Frame(window, bg="lightblue")
+    fra4 = Frame(window, bg="lightblue")
+    fra5 = Frame(window, bg="lightblue")
 
     fra1.pack()
     fra2.pack()
@@ -229,80 +229,73 @@ def dob():
     bu1.bind("<Button-1>",proverka)
     bu2.bind("<Button-1>",out)
 
-def uzm():
-    c=123
+def vu(database,p): #переименовать
 
-def cle():
-    c=123
+    output.delete(0,END)
 
-def vu(s,p):
-
-    lis.delete(0,END)
-
-    k = len(str(len(s)))
+    k = len(str(len(database)))
     z = (k-1)*' '+'№'
     z += 10*' '+'Фамилия'
     z += 18*' '+'Город'
     z += 7*' '+'Номер телефона'
     z += 2*' '+'Дата рождения'
-    la2.configure(text=z)
-    ent.configure(width=k+70)
-    lis.configure(width=k+76)
+    line_columns.configure(text=z)
+    line_found.configure(width=k+70)
+    output.configure(width=k+76)
     for i in p:
         lk = (k-len(str(i+1)))*' '+str(i+1)
-        lk += (17-len(s[i][0]))*' '+s[i][0]
-        lk += (23-len(s[i][1]))*' '+s[i][1]
-        lk += (21-len(s[i][2]))*' '+s[i][2]
-        lk += 5*' '+s[i][3]
-        lis.insert(END,lk)
+        lk += (17-len(database[i][0]))*' '+database[i][0]
+        lk += (23-len(database[i][1]))*' '+database[i][1]
+        lk += (21-len(database[i][2]))*' '+database[i][2]
+        lk += 5*' '+database[i][3]
+        output.insert(END,lk)
 
     
     
-from tkinter import *   # Подключаем модуль GUI
+from tkinter import *
 
-ok = Tk()   # Задаем главное окно
- 
-m = Menu(ok)        # Делаем в нем менюшку верхнюю
-ok.title("ПыЖиК")   # Наименование главного окна
-ok.config(menu=m)   # Связываем меню и окно
+main_window = Tk()
+main_window.title("ПыЖиК")
+main_menu = Menu(main_window)
+main_window.config(menu=main_menu)
 
-fm = Menu(m)        # Создается пункт меню с размещением на основном меню m
-m.add_cascade(label="File",menu=fm)             # Щелк и выпало менюшка...
-#!fm.add_command(label="Open...",command=Open)   Потом или никогда
-#!fm.add_command(label="New",command = New)      Потом или никогда
-#!fm.add_command(label="Save...",command = Save) Потом или никогда
-fm.add_command(label="Exit",command=Exit)       # Щелк, и все закрылось!
+first_menu = Menu(main_menu)
+main_menu.add_cascade(label="Файл",menu=first_menu)
+first_menu.add_command(label="Выход",command=exit)
+second_menu = Menu(main_menu)
+main_menu.add_cascade(label="Добавить запись",command=DOB)
 
-om = Menu(m) # Создается пункт меню с размещением на основном меню m
-m.add_cascade(label="Add enrry",command=dob) # Это добавление записи
-hm = Menu(m) # Создается пункт меню с размещением на основном меню m
-m.add_command(label="Help",command=Help) # Это хелпер
+third_menu = Menu(main_menu)
+main_menu.add_command(label="Справка",command=help)
 
-la1 = Label(ok,text="Поиск:", font="Courier 14")                    # Выводит "Поиск: "
-ent = Entry(ok,width=71, font="Courier 14")                         # Сама строка поиска.
-la2 = Label(ok,text='',font="Courier 14")                           # Строка на наименований столбцов.
-lis = Listbox(ok,selectmode=SINGLE,font="Courier 14",width=77)      # Вывод самых записей
+fourth_menu = Menu(main_menu)
+main_menu.add_command(label="О программе",command=about)
+
+poiskk = Label(main_window,text="Поиск:", font="Courier 14")
+line_found = Entry(main_window,width=71, font="Courier 14")
+line_columns = Label(main_window,text='',font="Courier 14")
+output = Listbox(main_window,selectmode=SINGLE,font="Courier 14",width=77)
 
 p = []
-for i in range(0,len(s)):
+for i in range(0,len(database)):
     p +=[i]
 
-vu(s,p)
+vu(database,p)
 
-scr = Scrollbar(ok,command=lis.yview)   # Создаем полоску прокрутки
-lis.configure(yscrollcommand=scr.set)   # Сустанавливаем связь между полоской прокрутки и списком
+scr = Scrollbar(main_window,command=output.yview)   # Создаем полоску прокрутки
+output.configure(yscrollcommand=scr.set)   # устанавливаем связь между полоской прокрутки и списком
 
-la1.grid(row=0,column=0)                            # В В Э Е
-ent.grid(row=0,column=1,columnspan=2)               # Ы С Т Р
-la2.grid(row=1,column=0,columnspan=2)               # В Е О Е
-lis.grid(row=2,column=0,rowspan=6,columnspan=3)     # О Й Й C
-scr.grid(row=2,column=4,rowspan=6)                  # Д     И
+poiskk.grid(row=0,column=0)                             # В В Э Е
+line_found.grid(row=0,column=1,columnspan=2)            # Ы С Т Р
+line_columns.grid(row=1,column=0,columnspan=2)          # В Е О Е
+output.grid(row=2,column=0,rowspan=6,columnspan=3)      # О Й Й C
+scr.grid(row=2,column=4,rowspan=6)                      # Д     И
 
-ent.bind("<Return>",poisk)
+line_found.bind("<Return>",poisk)
 
-#ok.maxsize(width = 880,height = 250)
-ok.minsize(width = 880,height = 250)
-ok.mainloop()   # Что-то, что все-таки создает наше окно
+main_window.maxsize(width = 890,height = 275)
+main_window.minsize(width = 890,height = 275)
+main_window.mainloop()   # Что-то, что все-таки создает наше окно
 
 # Хотелось бы переписать все классами
 # Понять как менять шрифт
